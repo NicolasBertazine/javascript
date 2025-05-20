6707c10816e7a21b9425da402d00ee8bd2b236fb
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-05-20 09:06:16 -0300

    Arrumando

--- DESAFIOS/desafio-11-situação-aluno/script.js
+++ DESAFIOS/desafio-11-situação-aluno/script.js
@@ -10,16 +10,16 @@ function verificar() {
     if (media > 6) {
         res.innerHTML = `<h1>Analisando a situação de ${nome}</h1>
     <p>Com as notas ${p1} e ${p2}, a <strong>média é ${media.toFixed(1)}</strong></p>
-    <p>Com a média acima de 6.0, o aluno está <strong>APROVADO</strong>.</p>`
+    <p>Com a média acima de 6.0, o aluno está <span style="background: green"><strong>APROVADO</strong></span></p>`
         
     } else if (media > 3) {
         res.innerHTML = `<h1>Analisando a situação de ${nome}</h1>
     <p>Com as notas ${p1} e ${p2}, a <strong>média é ${media.toFixed(1)}</strong></p>
-    <p>Com a média entre de 3.0 e 6.0, o aluno está em <strong>RECUPEREÇÃO</strong>.</p>`
+    <p>Com a média entre de 3.0 e 6.0, o aluno está em <span style="background: orange"><strong>RECUPEREÇÃO</strong></span></p>`
     } else {
         res.innerHTML = `<h1>Analisando a situação de ${nome}</h1>
     <p>Com as notas ${p1} e ${p2}, a <strong>média é ${media.toFixed(1)}</strong></p>
-    <p>Com a média abaixo de 3.0, o aluno está <strong>REPROVADO</strong>.</p>`
+    <p>Com a média abaixo de 3.0, o aluno está <span style="background: red"><strong>REPROVADO</strong></span></p>`
     }
 
 
